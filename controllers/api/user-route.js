a896339b9226352ca2d16c4ea4624e865401b858
Beatriz Bravo <beatz.bravo@gmail.com>
2024-01-10 21:24:29 -0700

    can log in and out with its specific routes and views

--- controllers/api/user-route.js
+++ controllers/api/user-route.js
@@ -14,4 +14,35 @@ router.get("/", async (req, res) => {
   res.status(200).json(allUsers);
 });
 
+router.post("/login", async (req, res) => {
+  try {
+    //find the user with the username
+    const userData = await User.findOne({
+      where: { username: req.body.username },
+    });
+    console.log(userData);
+    //create a session
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.json({ user: userData, message: "You are now logged in!" });
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//route for loging out
+router.post("/logout", (req, res) => {
+  if (req.session.logged_in) {
+    // Remove the session variables
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
 module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,19 +3,33 @@ const { User, Post, Comment } = require("../models");
 
 // GET all posts
 router.get("/", async (req, res) => {
-  const allPosts = await Post.findAll({
-    include: [
-      { model: User, as: "user" },
-      //   { model: Comment, as: "comments" },
-    ],
-  });
+  try {
+    const allPosts = await Post.findAll({
+      include: [
+        { model: User, as: "user" },
+        //   { model: Comment, as: "comments" },
+      ],
+    });
 
-  const posts = allPosts.map((post) => {
-    console.log(post.getDate);
-    return post.get({ plain: true });
-  });
-  console.log(posts);
-  res.render("homepage", { posts });
+    const posts = allPosts.map((post) => {
+      // console.log(post.getDate);
+      return post.get({ plain: true });
+    });
+    //   console.log(posts);
+    res.render("homepage", { posts, logged_in: req.session.logged_in });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//login page
+router.get("/login", (req, res) => {
+  //if a session exists, redirect to the homepage
+  if (req.session.logged_in) {
+    res.direct("/");
+    return;
+  }
+  res.render("login");
 });
 
 module.exports = router;
