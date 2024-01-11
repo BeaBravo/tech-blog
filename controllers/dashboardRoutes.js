ea32629bdf599d81fa0a31ed4120ad54a66661a5
Beatriz Bravo <beatz.bravo@gmail.com>
2024-01-10 21:38:18 -0700

    created middleware withAuth to check if a session exists. to be used in multiple places

---
+++ controllers/dashboardRoutes.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+const { User, Post, Comment } = require("../models");
+const withAuth = require("../utils/auth");
+
+router.get("/", withAuth, (req, res) => {
+  res.render("dashboard");
+});
+
+module.exports = router;
